92d2d7b094639cd5f61f6c491b2ede35cec561da
Gaubee <gaubeebangeel@gmail.com>
2025-06-05 23:57:32 +0800

    ♻️ 改进init指令，避免覆盖已有文件

--- packages/cli/src/commands/init.ts
+++ packages/cli/src/commands/init.ts
@@ -1,30 +1,44 @@
-import {writeJson, writeMarkdown} from "@gaubee/nodekit";
-import {str_trim_indent} from "@gaubee/util";
+import { writeJson, writeMarkdown } from "@gaubee/nodekit";
+import { str_trim_indent } from "@gaubee/util";
 import fs from "node:fs";
 import path from "node:path";
-import type {JixoConfig} from "../config.js";
+import type { JixoConfig } from "../config.js";
 export const init = (dir: string) => {
   const jixoDirname = path.join(dir, ".jixo");
   /// 创建 .jixo 目录
-  fs.mkdirSync(jixoDirname, {recursive: true});
-  writeMarkdown(
-    path.join(jixoDirname, "readme.task.md"),
-    str_trim_indent(`
+  fs.mkdirSync(jixoDirname, { recursive: true });
+  {
+    const readmeTaskFilepath = path.join(jixoDirname, "readme.task.md");
+    if (!fs.existsSync(readmeTaskFilepath)) {
+      writeMarkdown(
+        readmeTaskFilepath,
+        str_trim_indent(`
     <!-- 您可以自定义 readme.md 文件的格式 -->
     请JIXO帮我生成或者追加 README 文件
     `),
-    {
-      agents: ["readme-writer"],
-    },
-  );
+        {
+          agents: ["readme-writer"],
+        }
+      );
+    }
+  }
   /// 配置文件
-  writeJson(path.join(dir, "jixo.config.json"), {
-    tasks: {type: "dir", dirname: ".jixo"},
-  } satisfies JixoConfig);
-  /// .gitignore
+  {
+    const jixoConfigFilepath = path.join(jixoDirname, "jixo.config.json");
+    if (!fs.existsSync(jixoConfigFilepath)) {
+      writeJson(jixoConfigFilepath, {
+        tasks: { type: "dir", dirname: ".jixo" },
+      } satisfies JixoConfig);
+    }
+  }
+  /// .jixo/.gitignore
   {
     const gitignoreFilepath = path.join(jixoDirname, ".gitignore");
-    const gitignoreLines = (fs.existsSync(gitignoreFilepath) ? fs.readFileSync(gitignoreFilepath, "utf-8") : "").split(/\n+/);
+    const gitignoreLines = (
+      fs.existsSync(gitignoreFilepath)
+        ? fs.readFileSync(gitignoreFilepath, "utf-8")
+        : ""
+    ).split(/\n+/);
     let changed = false;
     for (const line of ["*.jixo-memory.md"]) {
       if (!gitignoreLines.includes(line)) {
